bc583ec46aa995d99e5535f736948f6e357d7179
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-03-28 20:07:08 +0300

    Добавляет вывод уведомлений при ошибке

--- tasks/pug.mjs
+++ tasks/pug.mjs
@@ -3,9 +3,17 @@ import pug from "gulp-pug";
 import pugBEM from "pug-bem";
 import { isDev } from "./mode.mjs";
 import { Path } from "./_constants.mjs";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
 
 export function compilePug() {
 	return gulp.src(Path.PUG.src)
+		.pipe(plumber({
+			errorHandler: notify.onError(error => ({
+				title: "Компиляция Pug",
+				message: error.message
+			}))
+		}))
 		.pipe(pug({
 			pretty: isDev(),
 			plugins: [pugBEM]
