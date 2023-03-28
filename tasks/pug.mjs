16d20c0b5b9d26dc4513e0a891f60e33b73cc936
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-03-28 19:40:18 +0300

    Добавляет БЭМ плагин для pug

--- tasks/pug.mjs
+++ tasks/pug.mjs
@@ -1,11 +1,13 @@
 import gulp from "gulp";
 import pug from "gulp-pug";
+import pugBEM from "pug-bem";
 import { isDev } from "./mode.mjs";
 
 export function compilePug() {
 	return gulp.src("source/pages/*.pug")
 		.pipe(pug({
-			pretty: isDev()
+			pretty: isDev(),
+			plugins: [pugBEM],
 		}))
 		.pipe(gulp.dest("build/"));
 }
