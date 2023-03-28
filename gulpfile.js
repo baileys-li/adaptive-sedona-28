b3a157e9954f913757def43034f558b64123b84c
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-03-28 19:32:35 +0300

    Добавляет режим продакшена и режим разработки

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
 import gulp from "gulp";
 import pug from "gulp-pug";
 
+/**
+ * @param {"production" | "development"} mode
+ */
+function setMode(mode = "development") {
+	process.env.NODE_ENV = mode;
+}
+
+const isDev = () => process.env.NODE_ENV === "development";
+const isProd = () => !isDev();
+const setProd = async () => await setMode("production");
+const setDev = async () => await setMode("development");
+
 function compilePug() {
 	return gulp.src("source/pages/*.pug")
-		.pipe(pug())
+		.pipe(pug({
+			pretty: isDev()
+		}))
 		.pipe(gulp.dest("build/"));
 }
 
-export default compilePug;
+export default gulp.series(setDev, compilePug);
+export const build = gulp.series(setProd, compilePug);
