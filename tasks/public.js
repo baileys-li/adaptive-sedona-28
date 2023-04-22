81235fb128b9bfcd63f02f424229cc25903d04cd
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-04-22 23:02:42 +0300

    Combine copy tasks

--- tasks/public.js
+++ tasks/public.js
@@ -1,13 +1,13 @@
 import gulp from "gulp";
 import { Path } from "./_constants.mjs";
 
-export const copyAssets = () => gulp.src(Path.ASSETS).pipe(gulp.dest("build"));
+const FONT_FILES = [400, 700].map(
+	(weight) =>
+		`node_modules/@fontsource/pt-sans/files/pt-sans-cyrillic-${weight}-normal.woff2`
+);
 
-export function copyFonts() {
-	const fontFiles = [400, 700].map(
-		(weight) =>
-			`node_modules/@fontsource/pt-sans/files/pt-sans-cyrillic-${weight}-normal.woff2`
-	);
+const copyAssets = () => gulp.src(Path.ASSETS).pipe(gulp.dest("build"));
 
-	return gulp.src(fontFiles).pipe(gulp.dest("build/fonts"));
-}
+const copyFonts = () => gulp.src(FONT_FILES).pipe(gulp.dest("build/fonts"));
+
+export const copyStatic = gulp.parallel(copyAssets, copyFonts);
--- tasks/serve.mjs
+++ tasks/serve.mjs
@@ -2,7 +2,7 @@ import gulp from "gulp";
 import browserSync from "browser-sync";
 import { compilePug } from "./pug.mjs";
 import { Path } from "./_constants.mjs";
-import { copyAssets, copyFonts } from "./public.js";
+import { copyStatic } from "./public.js";
 import { compileSass } from "./styles.mjs";
 import { compileScripts } from "./scripts.mjs";
 import { processImages } from "./images.mjs";
@@ -26,13 +26,13 @@ async function serve() {
 	server.init(SERVER_OPTIONS);
 
 	gulp.watch([Path.PUG.watch, Path.DATA.watch], compilePug);
-	gulp.watch(Path.ASSETS, copyAssets);
+	gulp.watch(Path.ASSETS, copyStatic);
 	gulp.watch(Path.STYLES.watch, streamStyles);
 	gulp.watch(Path.SCRIPTS.watch, compileScripts);
 	gulp.watch(Path.IMAGES.src, processImages);
 }
 
-const initialBuild = gulp.parallel(compilePug, copyFonts, copyAssets, compileSass, compileScripts, processImages);
+const initialBuild = gulp.parallel(compilePug, copyStatic, compileSass, compileScripts, processImages);
 const startServer = gulp.series(initialBuild, serve);
 
 export { initialBuild, startServer };
