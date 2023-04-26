b79aae3df07a7f5d9b696bec63fd5e0dc9a8a098
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-04-26 16:23:07 +0300

    Simplify header markup and improve a11y

--- source/typescript/utils/initNav.ts
+++ source/typescript/utils/initNav.ts
@@ -1,13 +1,18 @@
-const BASE_CLASS = "main-nav",
-			navigation = document.querySelector(`.${BASE_CLASS}`),
+const BASE_CLASS = "header",
+			MEDIA_BP = '(min-width: 42rem)';
+
+const navigation = document.querySelector(`.${BASE_CLASS}__nav`),
 			toggle = navigation?.querySelector<HTMLButtonElement>(`.${BASE_CLASS}__toggle`),
 			list = navigation?.querySelector<HTMLUListElement>(`.${BASE_CLASS}__list`);
 
+const mediaQuery = window.matchMedia(MEDIA_BP);
+
 if (navigation && toggle && list) {
 	toggle.hidden = false;
-	navigation.classList.add(`${BASE_CLASS}--enabled`);
+	navigation.classList.add(`${BASE_CLASS}__nav--enabled`);
 
 	toggle.addEventListener("click", toggleNav);
+	mediaQuery.addEventListener('change', correctNavInert)
 }
 
 function toggleNav() {
@@ -16,4 +21,9 @@ function toggleNav() {
 	toggle!.setAttribute('aria-expanded', String(isWillBeOpen));
 	toggle!.ariaLabel = toggle!.title = `${isWillBeOpen ? 'Закрыть' : 'Открыть'} меню`;
 	list!.inert = !isWillBeOpen;
+}
+
+function correctNavInert() {
+	const isClose = toggle!.getAttribute('aria-expanded') === 'false';
+	list!.inert = !mediaQuery.matches && isClose;
 }
